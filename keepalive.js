76d145bc573718d9a8a63254d8e576635da846cd
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-10-08 20:13:04 +0800

    Delete keepalive.js

--- keepalive.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => res.send("✅ Rafzbotz is alive!"));
-
-// Optional: ping diri sendiri tiap 5 menit biar gak sleep
-setInterval(() => {
-  require("node-fetch")(`https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`).catch(() => {});
-}, 5 * 60 * 1000);
