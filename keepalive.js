e6896843d799a3c2b55849a2ce59c6ab3aad56ef
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-10-08 20:10:03 +0800

    Update keepalive.js

--- keepalive.js
+++ keepalive.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("✅ Rafzbotz is alive!"));
-app.listen(3000, () => console.log("Keepalive server running on port 3000"));
 
 // Optional: ping diri sendiri tiap 5 menit biar gak sleep
 setInterval(() => {
