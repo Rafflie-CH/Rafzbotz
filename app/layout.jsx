d838363b7b5f2e8e71691c3862bf0e88b8bd9c62
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-12-26 23:57:51 +0800

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "RafzBot Dashboard",
+  description: "WA Bot Control Panel"
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
