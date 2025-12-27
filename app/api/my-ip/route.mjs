5396172609bd4a73ffaf502bf78da950a19a3d96
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-12-27 09:15:25 +0800

    Update route.mjs

--- app/api/my-ip/route.mjs
+++ app/api/my-ip/route.mjs
@@ -1,8 +1,5 @@
-module.exports = async (req) => {
-  const ip =
-    req.headers.get("x-forwarded-for")?.split(",")[0] ||
-    req.headers.get("x-real-ip") ||
-    "unknown"
-
-  return Response.json({ ip })
+export async function GET(req) {
+  return Response.json({
+    ip: req.headers.get("x-forwarded-for") || "Unknown"
+  })
 }
